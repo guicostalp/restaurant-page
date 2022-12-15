90473a8e705c9f4db1342fe8c14ffea213c46748
Gui Costa <gui.costalp@gmail.com>
2022-12-16 07:53:15 +1300

    Landing page bundle with webpack

---
+++ src/landing-page.js
@@ -0,0 +1,20 @@
+function createDiv () {
+
+    const div = document.createElement("div"); //create tag
+    div.setAttribute("id", "landing-page"); //set ID
+    div.setAttribute("class", "landing-page"); //set CLASS
+
+    //Create DIV Content
+    let createText = '';
+    createText += '<div  class="header">This is the Header and it is awesome</div>'
+    createText += '<h2> This is a wonderfull restaurant. The website is even better</h2>'
+
+    //Insert content inside DIV
+    div.innerHTML = createText;
+
+    //appendChild to div ID books
+    document.getElementById("container").appendChild(div);
+    
+} 
+
+export { createDiv };
