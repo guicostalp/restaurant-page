401f1bf4b6e52db1d66fc5ea901d2b7a7ff074cd
Gui Costa <gui.costalp@gmail.com>
2022-12-16 07:30:51 +1300

    Landing page bones done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log("Im alive!")
+
+function createDiv () {
+
+    const div = document.createElement("div"); //create tag
+    div.setAttribute("id", "landing-page"); //set ID
+    div.setAttribute("class", "landing-page"); //set CLASS
+
+    //Create DIV Content
+    let createText = '';
+    createText += '<div  class="header">This is the Header and it is awesome</div>'
+    createText += '<h2> This is a wonderfull restaurant. The website is even better</h2>'
+
+    //Insert content inside DIV
+    div.innerHTML = createText;
+
+    //appendChild to div ID books
+    document.getElementById("container").appendChild(div);
+    
+} 
+
+createDiv();
